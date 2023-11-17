fd1db81dbb7ce9d1da81eb64ce953746496cfca9
Y. Dogu <135986694+Vikipolimer@users.noreply.github.com>
2023-11-17 13:53:12 +0100

    Update Gadget-Adiutor.js
    This update introduces significant improvements to the Adiutor script, focusing on enhanced error handling and comprehensive documentation. Key enhancements include the addition of failure callbacks in updateOptions and processTranslation for robust error reporting, detailed comments for better clarity and maintainability of the code, refined handling of default user options to ensure stability, and a fallback to English translations to accommodate diverse user language settings.

--- Gadget-Adiutor.js
+++ Gadget-Adiutor.js
@@ -3,48 +3,57 @@
  * Learn more at: https://meta.wikimedia.org/wiki/Adiutor
  * License: Licensed under Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 <nowiki> */
+
 var api = new mw.Api();
 var wikiOptions = 'userjs-adiutor-' + mw.config.get('wgWikiID');
+
 // Function to update user options
+// This function sends updated options to the server via the API.
 function updateOptions(options) {
-	api.postWithEditToken({
-		action: 'globalpreferences',
-		format: 'json',
-		optionname: wikiOptions,
-		optionvalue: JSON.stringify(options),
-		formatversion: 2,
-	}, function() {});
-}
-// Function to update translations
-function updateTranslations() {
-	var jsonData = require('./Adiutor-i18.json');
-	if(typeof jsonData === 'object') {
-		for(var langCode in jsonData) {
-			if(jsonData.hasOwnProperty(langCode) && langCode !== '@metadata') {
-				// Pass necessary data as arguments to the function
-				processTranslation(langCode, jsonData[langCode]);
-			}
-		}
-	} else {
-		console.error('JSON content is not an object:', jsonData);
-	}
+    api.postWithEditToken({
+        action: 'globalpreferences',
+        format: 'json',
+        optionname: wikiOptions,
+        optionvalue: JSON.stringify(options),
+        formatversion: 2,
+    }).fail(function(err) {
+        console.error('Failed to update options:', err);
+    });
 }
 
-function processTranslation(langCode, translationData) {
-	var optionValue = JSON.stringify(translationData);
-	api.postWithEditToken({
-		action: 'globalpreferences',
-		format: 'json',
-		optionname: 'userjs-adiutor-i18-' + langCode,
-		optionvalue: optionValue,
-		formatversion: 2
-	}).done(function(response) {
-		console.log('Updated translation for langCode: ' + langCode);
-	}).fail(function(err) {
-		console.error('Failed to update translation for langCode: ' + langCode, err);
-	});
+// Function to update translations
+// This function handles updating translations by reading from a JSON file.
+function updateTranslations() {
+    var jsonData = require('./Adiutor-i18.json');
+    if (typeof jsonData === 'object') {
+        for (var langCode in jsonData) {
+            if (jsonData.hasOwnProperty(langCode) && langCode !== '@metadata') {
+                processTranslation(langCode, jsonData[langCode]);
+            }
+        }
+    } else {
+        console.error('JSON content is not an object:', jsonData);
+    }
 }
+
+// Processes individual translations and updates them via the API.
+function processTranslation(langCode, translationData) {
+    var optionValue = JSON.stringify(translationData);
+    api.postWithEditToken({
+        action: 'globalpreferences',
+        format: 'json',
+        optionname: 'userjs-adiutor-i18-' + langCode,
+        optionvalue: optionValue,
+        formatversion: 2
+    }).done(function(response) {
+        console.log('Updated translation for langCode:', langCode);
+    }).fail(function(err) {
+        console.error('Failed to update translation for langCode:', langCode, err);
+    });
+}
+
 // Define default user options for the Adiutor gadget
+// These are the predefined settings that will be used if the user has no saved settings.
 var adiutorUserOptionsDefault = {
 	"myWorks": [],
 	"myCustomSummaries": [],
@@ -100,51 +109,55 @@ var adiutorUserOptionsDefault = {
 	},
 	"adiutorVersion": "v2.0.0"
 };
+
 // Get user options related to the Adiutor gadget
-var adiutorUserOptions = JSON.parse(mw.user.options.get(wikiOptions));
+// This retrieves the current settings for the user from the server.
+var adiutorUserOptions = JSON.parse(mw.user.options.get(wikiOptions) || null);
+
 // Check if user options are not present or empty
-if(!adiutorUserOptions || Object.keys(adiutorUserOptions).length === 0) {
-	// Send default user options to the server using API
-	updateOptions(adiutorUserOptionsDefault);
-	// Retrieve default translation data
-	updateTranslations();
+// If the settings are not found or are empty, the defaults are set.
+if (!adiutorUserOptions || Object.keys(adiutorUserOptions).length === 0) {
+    updateOptions(adiutorUserOptionsDefault);
+    updateTranslations();
 } else {
-	var hasNewOptions = false; // Flag to check if there are new options
-	// Loop through the properties in adiutorUserOptionsDefault
-	for(var key in adiutorUserOptionsDefault) {
-		if(adiutorUserOptionsDefault.hasOwnProperty(key)) {
-			// Check if the property exists in adiutorUserOptions
-			if(!adiutorUserOptions.hasOwnProperty(key)) {
-				// New setting found, set the flag
-				hasNewOptions = true;
-				adiutorUserOptions[key] = adiutorUserOptionsDefault[key]; // Add the new option
-			}
-		}
-	}
-	// Update user options if new settings are found
-	if(hasNewOptions) {
-		updateOptions(adiutorUserOptions);
-		updateTranslations();
-	}
+    var hasNewOptions = false;
+    // Loop through default settings and add any missing settings to the user's current options.
+    for (var key in adiutorUserOptionsDefault) {
+        if (adiutorUserOptionsDefault.hasOwnProperty(key) && !adiutorUserOptions.hasOwnProperty(key)) {
+            hasNewOptions = true;
+            adiutorUserOptions[key] = adiutorUserOptionsDefault[key];
+        }
+    }
+    // Update the user's settings if new options were added.
+    if (hasNewOptions) {
+        updateOptions(adiutorUserOptions);
+        updateTranslations();
+    }
 }
+
+// Error handling and setting up translations
 try {
-	var userLanguage = mw.config.get('wgUserLanguage'); // Get user's language
-	var adiutorUserInterfaceTranslations = mw.user.options.get('userjs-adiutor-i18-' + userLanguage); // Get translation for user's language
-	// If there is no translation, use English as a fallback.
-	if(!adiutorUserInterfaceTranslations) {
-		adiutorUserInterfaceTranslations = mw.user.options.get('userjs-adiutor-i18-en');
-	}
-	// Ensure messages is an object with valid translations.
-	var messages = JSON.parse(adiutorUserInterfaceTranslations);
-	if(typeof messages !== 'object' || Object.keys(messages).length === 0) {
-		throw new Error('Invalid or empty translations');
-	}
-	// If so, work with the messages object.
-	mw.messages.set(messages);
-	// Load the Adiutor interface launcher
-	const AIL = require('./Adiutor-AIL.js');
-	AIL.callBack();
-} catch(error) {
-	console.error('Error fetching and processing translations:', error);
+    var userLanguage = mw.config.get('wgUserLanguage');
+    var adiutorUserInterfaceTranslations = mw.user.options.get('userjs-adiutor-i18-' + userLanguage);
+
+    // Use English as a fallback if no translation is available for the user's language.
+    if (!adiutorUserInterfaceTranslations) {
+        adiutorUserInterfaceTranslations = mw.user.options.get('userjs-adiutor-i18-en');
+    }
+
+    // Parse and set the translations for the interface.
+    var messages = JSON.parse(adiutorUserInterfaceTranslations || '{}');
+    if (typeof messages !== 'object' || !messages || Object.keys(messages).length === 0) {
+        throw new Error('Invalid or empty translations');
+    }
+
+    mw.messages.set(messages);
+
+    // Load the Adiutor Interface Launcher
+    const AIL = require('./Adiutor-AIL.js');
+    AIL.callBack();
+} catch (error) {
+    console.error('Error fetching and processing translations:', error);
 }
+
 /* </nowiki> */
